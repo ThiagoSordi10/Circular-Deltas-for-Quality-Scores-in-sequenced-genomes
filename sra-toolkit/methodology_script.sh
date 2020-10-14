b6c1af066fb488741b7c75b3d6492fbf80c3631c
Augusto Gai Dal'Asta <5328834-agasta@users.noreply.gitlab.com>
2020-10-14 17:16:54 -0300

    now counts the accessions

--- sra-toolkit/methodology_script.sh
+++ sra-toolkit/methodology_script.sh
@@ -20,6 +20,7 @@ echo "OK."
 
 rm ./$OUTPUT
 
+NUM_ACCESSION=1
 # for each accession id in the list
 for ACCESSION in `cat $INPUT`
 do
@@ -28,7 +29,7 @@ do
 	# calculate a hash (int) based on the id to use as a seed of a random number generator
 	HASH=`cksum <<< $ACCESSION | cut -f 1 -d ' '`
 	# print the info about the genome (accession id, hash, and number of entries available)
-	echo "### The accession $ACCESSION (with hash/seed $HASH) contains $NUM_ENTRIES FASTQ entries."
+	echo "$NUM_ACCESSION. The accession $ACCESSION (with hash/seed $HASH) contains $NUM_ENTRIES FASTQ entries."
 	# use the hash as the seed of the RANDOM bash variable
 	RANDOM=$HASH
 	# fetch $NUM_READS_PER_GENOME entries from each genome
@@ -40,6 +41,8 @@ do
 		# get the FASTQ entry number $ENTRY from the genome $ACCESSION and store it in the output file
 		fastq-dump.2.10.8 -N $ENTRY -X $ENTRY --skip-technical -Z $ACCESSION >> $OUTPUT 2>/dev/null
 	done
+
+	((NUM_ACCESSION=NUM_ACCESSION+1))
 done
 
 while read comment1; read dna; read comment2; read qs
