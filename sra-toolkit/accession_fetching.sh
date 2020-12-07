75bb85c76e21a46cbca4b90452f4aebe1d8fb162
Augusto Gai Dal'Asta <5328834-agasta@users.noreply.gitlab.com>
2020-12-07 15:58:00 -0300

    fixes

--- sra-toolkit/accession_fetching.sh
+++ sra-toolkit/accession_fetching.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ "$#" -ne "4" ]; then
+if [ "$#" -ne "5" ]; then
     echo "Error. Not enough arguments."
-	echo "EXAMPLE: ./accession_fetching.sh [integer]<number of reads per genome> [file]input.txt [file]output.txt [file]failed_accessions_output.txt"
+	echo "EXAMPLE: ./accession_fetching.sh [integer]<number of reads per genome> [boolean]<prefetched> [file]input.txt [file]output.txt [file]failed_accessions_output.txt"
     exit 1
 fi
 echo "Enough arguments supplied. Ready to go."
@@ -9,12 +9,14 @@ echo
 
 # number of fastq entries to fetch from each genome
 NUM_READS_PER_QUERY=$1
+# is it prefetched or not?
+PREFETCHED=$2
 # input file
-INPUT=$2
+INPUT=$3
 # and the output file
-OUTPUT=$3
+OUTPUT=$4
 # and a file specifically for failed accessions
-FAILED_ACCESSIONS_OUTPUT=$4
+FAILED_ACCESSIONS_OUTPUT=$5
 
 # this remove duplicate files, so that all the reads we do are not appended to old read files
 remove_duplicate_files () {
@@ -27,12 +29,18 @@ remove_duplicate_files () {
 fetch_accession() {
 	NUM_ACCESSIONS=1
 	# for each accession id in the list
-	for ACCESSION in `cat $INPUT`
+	for LINE in `cat $INPUT`
 	do
 		# disable_connections "1"
-
-		# get the number of FASTQ entries on that file
-		NUM_ENTRIES=`vdb-dump --id_range $ACCESSION | awk '{print $7}' | tr -d ","`
+		if [ ! $PREFETCHED ] 
+		then
+			# get the number of FASTQ entries on that file
+			ACCESSION=`echo $LINE`
+			NUM_ENTRIES=`vdb-dump --id_range $ACCESSION | awk '{print $7}' | tr -d ","`
+		else
+			ACCESSION=`echo $LINE | tr "," " " | awk '{print $1}'`
+        	NUM_ENTRIES=`echo $LINE | tr "," " " | awk '{print $2}'`
+		fi
 		
 		if [ -z  "$NUM_ENTRIES" ]; then
 			fetch_accession_failed
@@ -50,12 +58,16 @@ fetch_accession() {
 generate_entries_for_accession() {
 	# calculate a hash (int) based on the id to use as a seed of a random number generator
 	HASH=`cksum <<< $ACCESSION | cut -f 1 -d ' '`
-	# print the info about the genome (accession id, hash, and number of entries available)
-	echo "vdb_dump successful on accession number $NUM_ACCESSIONS." 
-	echo "The accession $ACCESSION (with hash/seed $HASH) contains $NUM_ENTRIES FASTQ entries."
+	
+	if [ ! $PREFETCHED ]
+	then
+		# print the info about the genome (accession id, hash, and number of entries available)
+		echo "vdb_dump successful on accession number $NUM_ACCESSIONS."
+		echo "The accession $ACCESSION (with hash/seed $HASH) contains $NUM_ENTRIES FASTQ entries."
+	fi
 
 	# calculate a random entry number (smaller than the total number of entries available in the genome)
-    ENTRIES=`gcc -o random random.c && ./random $ACCESSION $NUM_ENTRIES $HASH $NUM_READS_PER_QUERY`
+    ENTRIES=`./random $ACCESSION $NUM_ENTRIES $HASH $NUM_READS_PER_QUERY`
     echo "$ENTRIES" >> $OUTPUT
 }
 
@@ -71,5 +83,7 @@ fetch_accession_failed() {
 # OUR CODE STARTS HERE
 remove_duplicate_files $OUTPUT
 remove_duplicate_files $FAILED_ACCESSIONS_OUTPUT
+
+`gcc -o random random.c`
 
 fetch_accession
--- sra-toolkit/random.c
+++ sra-toolkit/random.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
     srand(atoi(argv[3]));
 
     for(int i = 0; i < atoi(argv[4]); i++) {
-        printf("%s %d\n", argv[1], rand() % atoi(argv[2]));
+        if(atoi(argv[2]) != 0)
+            printf("%s %d\n", argv[1], rand() % atoi(argv[2]));
     }
 }
