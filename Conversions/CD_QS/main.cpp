6ff71fde9e34560459213325fe7bddb6e7d0cfa9
Thiago Sordi <thiagosordi10@gmail.com>
2021-10-25 13:54:43 +0000

    Alteração Switch case para If

--- Conversions/CD_QS/main.cpp
+++ Conversions/CD_QS/main.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
@@ -7,22 +8,34 @@
 
 using namespace std;
 
-const vector<string> explode(const string& s, const char& c){
+// const vector<string> explode(const string& s, const char& c){
 
-	string buff{""};
-	vector<string> v;
+// 	string buff{""};
+// 	vector<string> v;
+
+// 	for(auto n:s){
+// 		if(n!=c){
+// 			buff+=n;
+// 		}
+// 		else if(n == c && buff != ""){
+// 			v.push_back(buff);
+// 			buff = "";
+// 		}
+// 	}
+// 	if(buff != ""){
+// 		v.push_back(buff);
+// 	}
+// 	return v;
+// }
+
+const vector<char> explode(const string& s, const char& c){
+
+	vector<char> v;
 
 	for(auto n:s){
 		if(n!=c){
-			buff+=n;
+			v.push_back(n);
 		}
-		else if(n == c && buff != ""){
-			v.push_back(buff);
-			buff = "";
-		}
-	}
-	if(buff != ""){
-		v.push_back(buff);
 	}
 	return v;
 }
@@ -32,7 +45,6 @@ int main(int argc, char *argv[]){
 	ifstream file(argv[1]);
 	//This is the new fastq data
 	ofstream fastq(argv[2]);
-
 	
 	if(file.is_open()){
 		string line;
@@ -43,14 +55,14 @@ int main(int argc, char *argv[]){
 			if(line_count == 4){
 				//Creates a vector that each part is a string with the number. EX: v[i] == # 
 				string linha = line;
-				vector<string> v{explode(linha, ' ')};
+				vector<char> v{explode(linha, ' ')};
 				//we write the first char value in the file
 				fastq << linha[0];
 				//transform the first ascii char to it's respective ascii value. EX: '#' == 35
 				int inicial = '0' + linha[0] - '0';
 				int valor_anterior;
 				int variavel_salva;
-				for(auto i = 1; i <v.size(); i++){
+				for(auto i = 1; i < v.size(); i++){
 					if(i == 1){
 						//The first iteration will read the first value of the line 61
 						valor_anterior = inicial;
@@ -60,136 +72,143 @@ int main(int argc, char *argv[]){
 						valor_anterior = variavel_salva;
 					}
 					//transform the char readed in the text file to a int value.
-					int valor = atoi(v[i].c_str());
-					//segunda parte do if é teste
-					if(valor_anterior -33 + valor > 41 || valor_anterior -33 + valor < 0){
-						switch(valor){
-							case -20:
-								valor = 21;
-								break;
-							case -19:
-								valor = 22;
-								break;
-							case -18:
-								valor = 23;
-								break;
-							case -17:
-								valor = 24;
-								break;
-							case -16:
-								valor = 25;
-								break;
-							case -15:
-								valor = 26;
-								break;
-							case -14:
-								valor = 27;
-								break;
-							case -13:
-								valor = 28;
-								break;
-							case -12:
-								valor = 29;
-								break;
-							case -11:
-								valor = 30;
-								break;
-							case -10:
-								valor = 31;
-								break;
-							case -9:
-								valor = 32;
-								break;
-							case -8:
-								valor = 33;
-								break;
-							case -7:
-								valor = 34;
-								break;
-							case -6:
-								valor = 35;
-								break;
-							case -5:
-								valor = 36;
-								break;
-							case -4:
-								valor = 37;
-								break;
-							case -3:
-								valor = 38;
-								break;
-							case -2:
-								valor = 39;
-								break;
-							case -1:
-								valor = 40;
-								break;
-							case 1:
-								valor = -40;
-								break;
-							case 2:
-								valor = -39;
-								break;
-							case 3:
-								valor =-38;
-								break;
-							case 4:
-								valor = -37;
-								break;
-							case 5:
-								valor = -36;
-								break;
-							case 6:
-								valor = -35;
-								break;
-							case 7:
-								valor = -34;
-								break;
-							case 8:
-								valor = -33;
-								break;
-							case 9:
-								valor = -32;
-								break;
-							case 10:
-								valor = -31;
-								break;
-							case 11:
-								valor = -30;
-								break;
-							case 12:
-								valor = -29;
-								break;
-							case 13:
-								valor = -28;
-								break;
-							case 14:
-								valor = -27;
-								break;
-							case 15:
-								valor = -26;
-								break;
-							case 16:
-								valor = -25;
-								break;
-							case 17:
-								valor = -24;
-								break;
-							case 18:
-								valor = -23;
-								break;
-							case 19:
-								valor = -22;
-								break;
-							case 20:
-								valor = -21;
-								break;
-						}
+					int valor = v[i]-75;
+          
+          if(valor_anterior - 33 + valor > 41){
+            valor -= 41;
+          }else if(valor_anterior - 33 + valor < 0){
+            valor += 41;
+          }
+
+					// if(valor_anterior - 33 + valor > 41 || valor_anterior -33 + valor < 0){
+          //   cout << valor;
+					// 	switch(valor){
+					// 		case -20:
+					// 			valor = 21;
+					// 			break;
+					// 		case -19:
+					// 			valor = 22;
+					// 			break;
+					// 		case -18:
+					// 			valor = 23;
+					// 			break;
+					// 		case -17:
+					// 			valor = 24;
+					// 			break;
+					// 		case -16:
+					// 			valor = 25;
+					// 			break;
+					// 		case -15:
+					// 			valor = 26;
+					// 			break;
+					// 		case -14:
+					// 			valor = 27;
+					// 			break;
+					// 		case -13:
+					// 			valor = 28;
+					// 			break;
+					// 		case -12:
+					// 			valor = 29;
+					// 			break;
+					// 		case -11:
+					// 			valor = 30;
+					// 			break;
+					// 		case -10:
+					// 			valor = 31;
+					// 			break;
+					// 		case -9:
+					// 			valor = 32;
+					// 			break;
+					// 		case -8:
+					// 			valor = 33;
+					// 			break;
+					// 		case -7:
+					// 			valor = 34;
+					// 			break;
+					// 		case -6:
+					// 			valor = 35;
+					// 			break;
+					// 		case -5:
+					// 			valor = 36;
+					// 			break;
+					// 		case -4:
+					// 			valor = 37;
+					// 			break;
+					// 		case -3:
+					// 			valor = 38;
+					// 			break;
+					// 		case -2:
+					// 			valor = 39;
+					// 			break;
+					// 		case -1:
+					// 			valor = 40;
+					// 			break;
+					// 		case 1:
+					// 			valor = -40;
+					// 			break;
+					// 		case 2:
+					// 			valor = -39;
+					// 			break;
+					// 		case 3:
+					// 			valor =-38;
+					// 			break;
+					// 		case 4:
+					// 			valor = -37;
+					// 			break;
+					// 		case 5:
+					// 			valor = -36;
+					// 			break;
+					// 		case 6:
+					// 			valor = -35;
+					// 			break;
+					// 		case 7:
+					// 			valor = -34;
+					// 			break;
+					// 		case 8:
+					// 			valor = -33;
+					// 			break;
+					// 		case 9:
+					// 			valor = -32;
+					// 			break;
+					// 		case 10:
+					// 			valor = -31;
+					// 			break;
+					// 		case 11:
+					// 			valor = -30;
+					// 			break;
+					// 		case 12:
+					// 			valor = -29;
+					// 			break;
+					// 		case 13:
+					// 			valor = -28;
+					// 			break;
+					// 		case 14:
+					// 			valor = -27;
+					// 			break;
+					// 		case 15:
+					// 			valor = -26;
+					// 			break;
+					// 		case 16:
+					// 			valor = -25;
+					// 			break;
+					// 		case 17:
+					// 			valor = -24;
+					// 			break;
+					// 		case 18:
+					// 			valor = -23;
+					// 			break;
+					// 		case 19:
+					// 			valor = -22;
+					// 			break;
+					// 		case 20:
+					// 			valor = -21;
+					// 			break;
+					// 	}
 						//int vaux = valor *-1;
 						//valor = vaux;
 						//valor = valor - 20;
 						//cout << valor << "|";
-					}
+			    // }
 					//Gets the sum of the value read and the last value
 					int variavel = valor_anterior + valor;
 					//Save the new value into a variable that will be used on line 71
